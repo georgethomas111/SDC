ba47d1094fa038aa27635befe04b8b28ebc4b2a2
George <georgethomas.mec@gmail.com>
2015-05-29 05:27:28 +0000

    Adding toInstall on a new VM

---
+++ toInstall.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install tmux
+sudo apt-get install htop
+sudo apt-get install g++
+sudo apt-get install vim 
+sudo apt-get install git
+sudo  apt-get install make
+sudo apt-get install xdg-utils
